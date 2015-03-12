27c06daa07b3699e6fad6f173de42c1c8b63486e
Eric Stensland <estensland88@gmail.com>
2015-03-12 08:47:33 -0500

    updating station_agent

--- app/controllers/station_agent_controller.rb
+++ app/controllers/station_agent_controller.rb
@@ -2,6 +2,7 @@ class StationAgentController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :sa_on?
 
   def show_all_models
     Rails.application.eager_load!
@@ -39,7 +40,7 @@ class StationAgentController < ApplicationController
     object.save
     redirect_to request.referer
   end
-  
+
   def new_anything
     @model = params['sa_model'].capitalize.constantize.new
 
@@ -47,7 +48,7 @@ class StationAgentController < ApplicationController
     @attr_hash = @model.class.columns_hash
     render :show_anything
   end
-  
+
   def create_anything
     attribs = params[params['sa_model'].downcase]
     object = params['sa_model'].new
@@ -79,7 +80,23 @@ class StationAgentController < ApplicationController
     end
   end
 
-  def yaml_file?
+  # yaml check
+
+  def sa_on?
+    return true unless has_yaml_file?
+
+    load_yaml
+
+    return false unless authenticate_user!
+    return false unless authenticate_method!(params[:action])
+    return false unless authenticate_model!(params[:sa_model])
+    return false unless authenticate_action_on_model!
+  end
+
+
+
+
+  def has_yaml_file?
     File.exist?("#{Rails.root}/config/station_agent.yml")
   end
 
@@ -87,8 +104,53 @@ class StationAgentController < ApplicationController
     @yaml = YAML.load_file("#{Rails.root}/config/station_agent.yml")
   end
 
-  def has_yaml_override?(model)
-    @yaml.has_key?(model)
+  def authenticate_user!
+    return true unless has_user_valid_check?
+    user_valid?
+  end
+
+  def authenticate_method!(method)
+    return true unless has_yaml_method_override_on? && has_method_override?(method)
+  end
+
+  def authenticate_model!(model)
+    return true unless has_yaml_model_override_on? && has_model_override?(model)
+  end
+
+  def authenticate_action_model!(model, action)
+    return true unless has_yaml_model_override_on? && has_model_override?(model)
+  end
+
+  def user_valid?
+    current_user.send(@yaml[:user_check_method])
+  end
+
+  def has_user_valid_check?
+    @yaml.has_key?(:user_check_method)
+  end
+
+  def has_yaml_method_override_on?
+    @yaml.has_key?('model_override')
+  end
+
+  def has_method_override?(method)
+    @yaml['model_override'].has_key?(method)
+  end
+
+  def has_yaml_model_override_on?
+    @yaml.has_key?('model_override')
+  end
+
+  def has_model_override?(model)
+    @yaml['model_override'].has_key?(model)
+  end
+
+  def has_yaml_action_on_model_override_on?
+    @yaml.has_key?('action_model_override')
+  end
+
+  def has_action_on_model_override?(model, action)
+    @yaml['action_model_override'].has_key?(model) && @yaml['action_model_override'][model].has_key?(action)
   end
 
   def has_yaml_attr?(yaml_data, first, array_value = nil)
