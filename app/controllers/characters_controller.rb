bf61be3553fdea666de960dd20a1673585ac1e8a
Eric Stensland <estensland88@gmail.com>
2015-03-16 15:23:22 -0500

    Update characters_controller.rb

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,4 +2,9 @@ class CharactersController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def index
+    params['offset'] ||= 0
+    @characters.limit(100).offset(params['offset'])
+  end
 end
