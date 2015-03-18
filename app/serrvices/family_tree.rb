16d96c9adc2ecd234cbdd9bb095776ca17e99436
Eric Stensland <estensland88@gmail.com>
2015-03-17 21:39:20 -0500

    Create family_tree.rb

---
+++ app/serrvices/family_tree.rb
@@ -0,0 +1,12 @@
+class FamilyTree
+  attr_reader: :chracter
+  def initialize(opts={})
+    @character = opts[:character]
+    @tree_json = generate_json
+  end
+  
+  def generate_json
+    @character.family
+  end
+
+end
