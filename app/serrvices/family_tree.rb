5fab8e03fcc9d5f1a082cb35eb224f6843ade94e
Eric Stensland <estensland88@gmail.com>
2015-03-19 07:56:15 -0500

    prepping the family tree service

--- app/serrvices/family_tree.rb
+++ app/serrvices/family_tree.rb
@@ -2,11 +2,13 @@ class FamilyTree
   attr_reader: :chracter
   def initialize(opts={})
     @character = opts[:character]
+    @generations_back = opts[:generations_back]
+    @generations_forward = opts[:generations_forward]
     @tree_json = generate_json
   end
 
   def generate_json
-    @character.family
+    @character.family(generations_forward: @generations_forward, generations_back: @generations_back)
   end
 
 end
