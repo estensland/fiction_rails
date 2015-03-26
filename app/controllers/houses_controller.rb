9cf9cd4d483c66b39f02e0436f9852c59f323c21
Eric Stensland <estensland88@gmail.com>
2015-03-26 07:46:44 -0500

    Update houses_controller.rb

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,4 +2,15 @@ class HousesController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def index
+    @page = params['page'']
+    @limit = params['limit']
+    @houses = House.limit(params['limit']).offset(params['offset'] * params['limit'])
+  end
+  
+  def show
+    @house = House.find(params['id'])
+    @characters = @house.characters
+  end
 end
