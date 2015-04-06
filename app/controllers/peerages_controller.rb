fee7d01ab64638bbf829a22168243cfc682a1920
Eric Stensland <estensland88@gmail.com>
2015-04-06 15:01:45 -0500

    Update peerages_controller.rb

--- app/controllers/peerages_controller.rb
+++ app/controllers/peerages_controller.rb
@@ -2,4 +2,9 @@ class PeeragesController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def index
+    params['offset'] ||= 0
+    @peerages = Peerage.limit(100).offset(params['offset'])
+  end
 end
