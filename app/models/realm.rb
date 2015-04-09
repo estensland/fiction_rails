ae6240ae6ecc6bd6ab769af0fbc47a984ac50d8a
Eric Stensland <estensland88@gmail.com>
2015-04-09 10:08:10 -0500

    Update realm.rb

--- app/models/realm.rb
+++ app/models/realm.rb
@@ -1,6 +1,9 @@
 class Realm < ActiveRecord::Base
   has_ancestry
-  belongs_to :title
+  belongs_to :title # Just the primary title
+  has_many :titles  # All of them
+  has_many :houses
+  has_many :characters, through: :houses
 
   def main_title
     if self.primary_title && self.primary_title.peerage
