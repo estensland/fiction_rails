91db92e1a04e1b5e7571b508749aebbae1c02c57
Eric Stensland <estensland88@gmail.com>
2015-04-11 13:46:20 -0500

    controllers for timeline set;

--- app/controllers/timeline_events_controller.rb
+++ app/controllers/timeline_events_controller.rb
@@ -3,14 +3,8 @@ class TimelineController < ApplicationController
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
-    params['offset'] ||= 0
-    @timelines = Timeline.limit(100).offset(params['offset'])
-  end
-
   def show
-    @timeline = Timeline.find(params['id'])
-    @events = @timeline.timeline_events
+    @event = TimelineEvent.find(params['id'])
   end
 
 end
