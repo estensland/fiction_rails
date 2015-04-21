15b59a073e34665808e05bd8bbff3cd71c776562
Eric Stensland <estensland88@gmail.com>
2015-04-21 07:07:10 -0500

    fixed misspelling of mgiration

--- db/migrate/20150416122413_create_timeline_event_relateds.rb
+++ db/migrate/20150416122413_create_timeline_event_relateds.rb
@@ -1,6 +1,6 @@
-class TimelineEventRelated < ActiveRecord::Migration
+class CreateTimelineEventRelateds < ActiveRecord::Migration
   def change
-    create_table :timeline_events do |t|
+    create_table :timeline_event_relateds do |t|
       t.integer :timeline_event_id
       t.integer :related_id
       t.integer :related_type
