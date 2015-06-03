b30723ffec763d9ce781b0047bcc36a87869049a
Eric Stensland <estensland88@gmail.com>
2015-06-03 07:22:15 -0500

    debugging angular talking to rails for controllers

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -2,5 +2,13 @@
 
 angular.module('myApp', [
   'ngRoute',
-  'myApp.home'
+  'myApp.home',
+  'myApp.character',
+  'myApp.characters',
+  'myApp.house',
+  'myApp.houses',
+  'myApp.realm',
+  'myApp.realms',
+  'myApp.timeline',
+  'myApp.timelines'
 ])
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,11 +1,11 @@
 class House < ActiveRecord::Base
-  has_many :primary_house_characters, class_name: :character
+  has_many :primary_house_characters, class_name: Character
   has_many :character_houses
   has_many :characters, through: :character_houses
 
   belongs_to :realm
 
   def api_ready
-    self.to_json(:methods => [:primary_house_characters, :characters, :realm])
+    self.to_json(:methods => [:primary_house_characters, :character_houses, :realm])
   end
 end
