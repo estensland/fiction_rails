950452d3afeb9cc16fa9cf0f4067957f11076574
Eric Stensland <estensland88@gmail.com>
2015-06-25 07:22:53 -0500

    Update app.js

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -6,11 +6,15 @@ angular.module('myApp', [
   'myApp.characterSearch',
   'myApp.characters',
   'myApp.character',
+  'myApp.language',
+  'myApp.languages',
   'myApp.house',
   'myApp.houses',
   'myApp.realm',
   'myApp.realms',
   'myApp.timeline',
   'myApp.timelines',
+  'myApp.word_count',
+  'myApp.word_counts',
   'myApp.directives.characterInfoPanel'
 ])
