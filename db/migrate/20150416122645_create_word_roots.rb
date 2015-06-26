ae78a2573fc2adc32aa79053371b6c31fc39edd8
Eric Stensland <estensland88@gmail.com>
2015-06-26 06:20:19 -0500

    Update 20150416122645_create_word_roots.rb

--- db/migrate/20150416122645_create_word_roots.rb
+++ db/migrate/20150416122645_create_word_roots.rb
@@ -1,8 +1,9 @@
 class CreateWordRoots < ActiveRecord::Migration
   def change
     create_table :word_roots do |t|
-      t.string :word_source
-      t.json :word_tributaries
+      t.string  :word_source
+      t.boolean :subconjugations
+      t.json    :word_tributaries
       t.integer :language_id
     end
   end
