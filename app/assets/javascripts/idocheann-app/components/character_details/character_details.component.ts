f7bd08d662451a393fe6c68fd0655d3cda2c7685
Eric Stensland <estensland88@gmail.com>
2016-06-18 08:25:46 -0500

    messing with tempaltes

--- app/assets/javascripts/idocheann-app/components/character_details/character_details.component.ts
+++ app/assets/javascripts/idocheann-app/components/character_details/character_details.component.ts
@@ -7,7 +7,7 @@ import { Router, RouterLink, RouteParams } from '@angular/router-deprecated';
   selector: 'character',
   template: `
     <h2>{{character.composite_name}}</h2>
-    <div class="col-md-6">
+    <div class="col-md-12">
       <table class="table table-striped table-bordered">
         <tr>
           <td><strong>Native Name:</strong></td>
@@ -34,8 +34,28 @@ import { Router, RouterLink, RouteParams } from '@angular/router-deprecated';
           <td>{{character.real_person}}</td>
         </tr>
         <tr>
+          <td><strong>Sex:</strong></td>
+          <td>{{character.sex}}</td>
+        </tr>
+        <tr>
           <td><strong>Fighting Class:</strong></td>
           <td>{{character.fighting_class}}</td>
+        </tr>
+        <tr>
+          <td><strong>Primary House:</strong></td>
+          <td>{{character.primary_house && character.primary_house.name}}</td>
+        </tr>
+        <tr>
+          <td><strong>Father:</strong></td>
+          <td>{{character.father && character.father.composite_name}}</td>
+        </tr>
+        <tr>
+          <td><strong>Mother:</strong></td>
+          <td>{{character.father && character.mother.composite_name}}</td>
+        </tr>
+        <tr *ngFor="let sib of character.siblings">
+          <td><strong>Sibling:</strong></td>
+          <td>{{sib.composite_name}}</td>
         </tr>
       </table>
     </div>
@@ -52,9 +72,11 @@ export class CharacterDetailsComponent implements OnInit {
   constructor(private characterService: CharacterService, params: RouteParams) { this.params = params }
 
   ngOnInit() {
-    if (!this.character) { this.character = {id: 1}}
+    if (!this.character) { this.character = {id: 1, siblings: [{}]}}
     this.characterService.get(this.params)
       .then(character => this.character = character).catch(error => this.error = error);
   }
+
+
 
 }
